2530e25eb83f91e108ff539562e3d29a8090bf2f
Shazzad-Shuvo <md.rahman.shazzadur@gmail.com>
2023-09-15 00:42:55 +0600

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 
+import { useState } from 'react'
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+import Header from './components/Header/Header'
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0);
+
+  const handleAddToBookmark = (blog) => {
+    const newBookmarks = [...bookmarks, blog];
+    setBookmarks(newBookmarks);
+  }
+
+  const handleMarkAsRead = (time) => {
+    const newReadingTime = readingTime + time;
+    setReadingTime(newReadingTime);
+    console.log(readingTime);
+  }
 
   return (
     <>
-      <h1 className='text-6xl font-bold bg-cyan-300'>Knowledge Cafe</h1>
-      
+      <Header></Header>
+      <div className='md:flex max-w-7xl mx-auto mt-8'>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
+
     </>
   )
 }
